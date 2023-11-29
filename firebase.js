3605b468645a5d7fd2ee4e0bb0e1ae5abbe8c2fa
khadir786 <52165828+khadir786@users.noreply.github.com>
2023-11-29 14:29:10 +0000

    removed config firebase.js

--- firebase.js
+++ firebase.js
@@ -7,15 +7,8 @@ import { getFirestore, collection } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBszxk6bB-CWIrnNfgw36imuK3Lh9lhRfY",
-  authDomain: "react-notes-6df06.firebaseapp.com",
-  projectId: "react-notes-6df06",
-  storageBucket: "react-notes-6df06.appspot.com",
-  messagingSenderId: "287241238786",
-  appId: "1:287241238786:web:6a1335aafcdd20d66b6a1b",
-  measurementId: "G-TJ8TMV1V6X"
-};
+
+// FIREBASE CONFIG HERE
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
